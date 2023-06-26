ce3492993ca41a1346b726d87a51d4c942a46ecb
Jatin rana <engr.jatinrana@gmail.com>
2023-06-26 17:27:27 +0530

    delete unnecessary code

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
